0dc0fa124385ecc593f0414be658dda6b8928c91
Mark Grover <mgrover@cloudera.com>
2015-08-06 09:45:43 -0700

    Adding a new ModifiedPi app that creates a lot of ByteBuffers Minor improvements to LotsOfByteBuffers

--- src/main/scala/com/markgrover/spark/LotsOfByteBuffers.scala
+++ src/main/scala/com/markgrover/spark/LotsOfByteBuffers.scala
@@ -12,11 +12,16 @@ object LotsOfByteBuffers {
   def main(args: Array[String]) {
     val sc = new SparkContext(new SparkConf().setAppName("Lots of ByteBuffers"))
     val split = sc.textFile(args(0)).flatMap(_.split(" "))
-    val capacity = if (args.length > 1) args(1).toInt else 100
+    val count = if (args.length > 1) args(1).toInt else 1
+    val capacity = if (args.length > 2) args(2).toInt else 100
 
     split.map(element => {
-      System.out.println(s"Allocating 1 buffer of $capacity capacity"); ByteBuffer.allocateDirect(
-        capacity)
+      System.out.println(s"Allocating $count buffers of $capacity capacity")
+      for (i <- 0 until count) {
+        ByteBuffer.allocateDirect(capacity)
+      }
     })
+    split.cache()
+    sc.stop()
   }
 }
